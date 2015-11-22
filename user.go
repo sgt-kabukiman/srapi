8587b2e2e90250bf1c248deb276b58c22af5596f
Sgt. Kabukiman <sgt@kabukiman.org>
2015-11-22 04:08:34 +0100

    embed support for PBs

--- user.go
+++ user.go
@@ -96,6 +96,190 @@ func (self *User) links() []Link {
 type PersonalBest struct {
 	Rank int
 	Run  Run
+
+	// do not use this field directly, use the available methods
+	PlatformData interface{} `json:"platform"`
+
+	// do not use this field directly, use the available methods
+	RegionData interface{} `json:"region"`
+
+	// do not use this field directly, use the available methods
+	PlayersData interface{} `json:"players"`
+
+	// do not use this field directly, use the available methods
+	GameData interface{} `json:"game"`
+
+	// do not use this field directly, use the available methods
+	CategoryData interface{} `json:"category"`
+
+	// do not use this field directly, use the available methods
+	LevelData interface{} `json:"level"`
+}
+
+func (self *PersonalBest) Game() *Game {
+	if self.GameData == nil {
+		return nil
+	}
+
+	// convert generic mess into JSON
+	dest := Game{}
+
+	if recast(self.GameData, &dest) == nil {
+		return &dest
+	}
+
+	return nil
+}
+
+func (self *PersonalBest) Category() *Category {
+	if self.CategoryData == nil {
+		return nil
+	}
+
+	// convert generic mess into JSON
+	dest := Category{}
+
+	if recast(self.CategoryData, &dest) == nil {
+		return &dest
+	}
+
+	return nil
+}
+
+func (self *PersonalBest) Level() *Level {
+	if self.LevelData == nil {
+		return nil
+	}
+
+	// convert generic mess into JSON
+	dest := Level{}
+
+	if recast(self.LevelData, &dest) == nil {
+		return &dest
+	}
+
+	return nil
+}
+
+func (self *PersonalBest) Platform() *Platform {
+	if self.PlatformData == nil {
+		if len(self.Run.System.Platform) > 0 {
+			platform, _ := PlatformById(self.Run.System.Platform)
+			return platform
+		}
+
+		return nil
+	}
+
+	// convert generic mess into JSON
+	dest := Platform{}
+
+	if recast(self.PlatformData, &dest) == nil {
+		return &dest
+	}
+
+	return nil
+}
+
+func (self *PersonalBest) Region() *Region {
+	if self.RegionData == nil {
+		if len(self.Run.System.Region) > 0 {
+			region, _ := RegionById(self.Run.System.Region)
+			return region
+		}
+
+		return nil
+	}
+
+	// convert generic mess into JSON
+	dest := Region{}
+
+	if recast(self.RegionData, &dest) == nil {
+		return &dest
+	}
+
+	return nil
+}
+
+func (self *PersonalBest) Players() []*Player {
+	result := make([]*Player, 0)
+
+	switch asserted := self.PlayersData.(type) {
+	// list of simple links to users/guests, e.g. players=[{rel:..,id:...}, {...}]
+	case []interface{}:
+		tmp := make([]PlayerLink, 0)
+
+		if recast(asserted, &tmp) == nil {
+			for _, link := range tmp {
+				player := Player{}
+
+				switch link.Relation {
+				case "user":
+					user, err := fetchUser(link.request(nil, nil))
+					if err == nil {
+						player.User = user
+					}
+
+				case "guest":
+					guest, err := fetchGuest(link.request(nil, nil))
+					if err == nil {
+						player.Guest = guest
+					}
+				}
+
+				if player.User != nil || player.Guest != nil {
+					result = append(result, &player)
+				}
+			}
+		}
+
+	// sub-resource due to embeds, aka "{data:....}"
+	case map[string]interface{}:
+		tmp := playerCollection{}
+
+		if recast(asserted, &tmp) == nil {
+			// each element in tmp.Data has a rel that tells us whether we have a
+			// user or a guest
+			for _, playerProps := range tmp.Data {
+				rel, exists := playerProps["rel"]
+				if exists {
+					player := Player{}
+
+					switch rel {
+					case "user":
+						user := User{}
+
+						if recast(playerProps, &user) == nil {
+							player.User = &user
+						}
+
+					case "guest":
+						guest := Guest{}
+
+						if recast(playerProps, &guest) == nil {
+							player.Guest = &guest
+						}
+					}
+
+					if player.User != nil || player.Guest != nil {
+						result = append(result, &player)
+					}
+				}
+			}
+		}
+	}
+
+	return result
+}
+
+func (self *PersonalBest) Examiner() *User {
+	link := firstLink(&self.Run, "examiner")
+	if link == nil {
+		return nil
+	}
+
+	examiner, _ := fetchUser(link.request(nil, nil))
+	return examiner
 }
 
 type personalBestResponse struct {
