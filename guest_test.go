79017c439a86fee521089b01ad206f5f44fff304
Sgt. Kabukiman <sgt@kabukiman.org>
2015-11-24 03:28:22 +0100

    add basic tests for guests

---
+++ guest_test.go
@@ -0,0 +1,36 @@
+// Copyright (c) 2015, Sgt. Kabukiman | MIT licensed
+
+package srapi
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestGuests(t *testing.T) {
+	Convey("Fetching valid guest names should succeed.", t, func() {
+		name := "SgtRockworth"
+
+		guest, err := GuestByName(name)
+
+		So(err, ShouldBeNil)
+		So(guest.Name, ShouldEqual, name)
+		So(guest.Links, ShouldNotBeEmpty)
+	})
+
+	Convey("Fetching unknown guests should fail.", t, func() {
+		guest, err := GuestByName("i9r29fgwiurh2igtrw89fw7f")
+		So(err, ShouldNotBeNil)
+		So(guest, ShouldBeNil)
+	})
+
+	Convey("Each guest should have a non-empty collection of runs.", t, func() {
+		guest, err := GuestByName("SgtRockworth")
+		So(err, ShouldBeNil)
+
+		runs := guest.Runs(nil, nil)
+		So(err, ShouldBeNil)
+		So(runs, ShouldNotBeEmpty)
+	})
+}
