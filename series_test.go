fb457afd3a4a754b165aaf6a45e8dbe335db48c2
Sgt. Kabukiman <sgt@kabukiman.org>
2015-11-25 01:18:10 +0100

    add series and variables tests

---
+++ series_test.go
@@ -0,0 +1,182 @@
+// Copyright (c) 2015, Sgt. Kabukiman | MIT licensed
+
+package srapi
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestSeries(t *testing.T) {
+	Convey("Fetching series by valid IDs", t, func() {
+		id := "9v7og6n0" // GTA
+
+		series, err := SeriesByID(id)
+
+		So(err, ShouldBeNil)
+		So(series.ID, ShouldEqual, id)
+		So(series.Names.International, ShouldEqual, "Grand Theft Auto")
+		So(series.Abbreviation, ShouldEqual, "gta")
+		So(series.Weblink, ShouldNotBeEmpty)
+		So(series.Links, ShouldNotBeEmpty)
+		So(series.ModeratorMap(), ShouldNotBeEmpty)
+
+		mods, err := series.Moderators()
+		So(err, ShouldBeNil)
+		So(mods, ShouldNotBeEmpty)
+	})
+
+	Convey("Fetching series by valid abbreviation", t, func() {
+		series, err := SeriesByAbbreviation("gta")
+
+		So(err, ShouldBeNil)
+		So(series.ID, ShouldEqual, "9v7og6n0")
+		So(series.Names.International, ShouldEqual, "Grand Theft Auto")
+		So(series.Abbreviation, ShouldEqual, "gta")
+		So(series.Weblink, ShouldNotBeEmpty)
+		So(series.Links, ShouldNotBeEmpty)
+		So(series.ModeratorMap(), ShouldNotBeEmpty)
+
+		mods, err := series.Moderators()
+		So(err, ShouldBeNil)
+		So(mods, ShouldNotBeEmpty)
+	})
+
+	Convey("Fetching series by invalid IDs", t, func() {
+		series, err := SeriesByID("i_do_not_exist")
+		So(err, ShouldNotBeNil)
+		So(series, ShouldBeNil)
+	})
+
+	Convey("Fetching series by invalid IDs", t, func() {
+		series, err := SeriesByAbbreviation("i_do_not_exist")
+		So(err, ShouldNotBeNil)
+		So(series, ShouldBeNil)
+	})
+
+	Convey("Fetching multiple series", t, func() {
+		Convey("starting from the beginning", func() {
+			seriesList, err := ManySeries(nil, nil, nil)
+			So(err, ShouldBeNil)
+			So(seriesList.Data, ShouldNotBeEmpty)
+			So(seriesList.Pagination.Offset, ShouldEqual, 0)
+
+			series := seriesList.Data[0]
+			So(series.ID, ShouldNotBeBlank)
+			So(series.Names.International, ShouldNotBeBlank)
+			So(series.Links, ShouldNotBeEmpty)
+		})
+
+		Convey("skipping the first few", func() {
+			seriesList, err := ManySeries(nil, nil, &Cursor{2, 0})
+			So(err, ShouldBeNil)
+			So(seriesList.Data, ShouldNotBeEmpty)
+			So(seriesList.Pagination.Offset, ShouldEqual, 2)
+			So(seriesList.Pagination.Links, ShouldNotBeEmpty)
+
+			series := seriesList.Data[0]
+			So(series.ID, ShouldNotBeBlank)
+			So(series.Names.International, ShouldNotBeBlank)
+			So(series.Links, ShouldNotBeEmpty)
+		})
+
+		Convey("limited to just a few", func() {
+			seriesList, err := ManySeries(nil, nil, &Cursor{0, 3})
+			So(err, ShouldBeNil)
+			So(seriesList.Data, ShouldHaveLength, 3)
+			So(seriesList.Pagination.Offset, ShouldEqual, 0)
+			So(seriesList.Pagination.Max, ShouldEqual, 3)
+			So(seriesList.Pagination.Links, ShouldNotBeEmpty)
+
+			series := seriesList.Data[0]
+			So(series.ID, ShouldNotBeBlank)
+			So(series.Names.International, ShouldNotBeBlank)
+			So(series.Links, ShouldNotBeEmpty)
+		})
+
+		Convey("paging through the seriesList", func() {
+			seriesList, err := ManySeries(nil, nil, &Cursor{0, 1})
+			So(err, ShouldBeNil)
+			So(seriesList.Data, ShouldHaveLength, 1)
+			So(seriesList.Pagination.Offset, ShouldEqual, 0)
+			So(seriesList.Pagination.Max, ShouldEqual, 1)
+
+			seriesList, err = seriesList.NextPage()
+			So(err, ShouldBeNil)
+			So(seriesList.Data, ShouldHaveLength, 1)
+			So(seriesList.Pagination.Offset, ShouldEqual, 1)
+			So(seriesList.Pagination.Max, ShouldEqual, 1)
+
+			seriesList, err = seriesList.NextPage()
+			So(err, ShouldBeNil)
+			So(seriesList.Data, ShouldHaveLength, 1)
+			So(seriesList.Pagination.Offset, ShouldEqual, 2)
+			So(seriesList.Pagination.Max, ShouldEqual, 1)
+
+			seriesList, err = seriesList.PrevPage()
+			So(err, ShouldBeNil)
+			So(seriesList.Data, ShouldHaveLength, 1)
+			So(seriesList.Pagination.Offset, ShouldEqual, 1)
+			So(seriesList.Pagination.Max, ShouldEqual, 1)
+		})
+
+		Convey("the prev page from the beginning should yield an error", func() {
+			seriesList, err := ManySeries(nil, nil, nil)
+
+			seriesList, err = seriesList.PrevPage()
+			So(err, ShouldNotBeNil)
+			So(seriesList, ShouldNotBeNil)
+		})
+
+		Convey("test the SeriesFilter", func() {
+			// check abbrevitation
+			filter := SeriesFilter{Abbreviation: "gta"}
+
+			seriesList, err := ManySeries(&filter, nil, nil)
+			So(err, ShouldBeNil)
+			So(seriesList.Data, ShouldHaveLength, 1)
+
+			// check name
+			filter = SeriesFilter{Name: "mario"}
+			cursor := Cursor{Max: 5}
+
+			seriesList, err = ManySeries(&filter, nil, &cursor)
+			So(err, ShouldBeNil)
+			So(seriesList.Data, ShouldHaveLength, 5)
+
+			// check moderator
+			filter = SeriesFilter{Moderator: "r5j52gjv"}
+
+			seriesList, err = ManySeries(&filter, nil, nil)
+			So(err, ShouldBeNil)
+			So(len(seriesList.Data), ShouldBeBetween, 2, 5) // Sorry Josh, but I don't assume it's gonna be more than 5 #Kappa
+		})
+	})
+
+	Convey("Fetching games of a series", t, func() {
+		series, err := SeriesByID("9v7og6n0") // Gameboy
+		So(err, ShouldBeNil)
+
+		games, err := series.Games(nil, nil)
+		So(err, ShouldBeNil)
+
+		firstID := ""
+
+		Convey("first page of games should be fine", func() {
+			So(games.Data, ShouldNotBeEmpty)
+			So(games.Pagination.Offset, ShouldEqual, 0)
+
+			firstID = games.Data[0].ID
+		})
+
+		games, err = series.Games(nil, &Sorting{Direction: Descending})
+		So(err, ShouldBeNil)
+
+		Convey("sorting order should be taken into account", func() {
+			So(games.Data, ShouldNotBeEmpty)
+			So(games.Pagination.Offset, ShouldEqual, 0)
+			So(firstID, ShouldNotEqual, games.Data[0].ID)
+		})
+	})
+}
---
+++ variable_test.go
@@ -0,0 +1,56 @@
+// Copyright (c) 2015, Sgt. Kabukiman | MIT licensed
+
+package srapi
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestVariables(t *testing.T) {
+	seedID := "0789j6nw" // gta vc chaos% seed
+	seedVar, err := VariableByID(seedID)
+
+	Convey("Fetching variables by valid IDs", t, func() {
+		So(err, ShouldBeNil)
+		So(seedVar.ID, ShouldEqual, seedID)
+		So(seedVar.Name, ShouldEqual, "Seed")
+		So(seedVar.Scope.Type, ShouldEqual, "full-game")
+		So(seedVar.Scope.Level, ShouldBeEmpty)
+		So(seedVar.Mandatory, ShouldBeTrue)
+		So(seedVar.UserDefined, ShouldBeTrue)
+		So(seedVar.Obsoletes, ShouldBeTrue)
+		So(seedVar.Values.Choices, ShouldNotBeEmpty)
+		So(seedVar.Values.Default, ShouldBeEmpty)
+		So(seedVar.Links, ShouldNotBeEmpty)
+	})
+
+	Convey("Fetching variables by invalid IDs", t, func() {
+		variable, err := VariableByID("i_do_not_exist")
+		So(err, ShouldNotBeNil)
+		So(variable, ShouldBeNil)
+	})
+
+	Convey("Fetch the game the variable belongs to", t, func() {
+		game, err := seedVar.Game()
+		So(err, ShouldBeNil)
+		So(game, ShouldNotBeNil)
+		So(game.Names.International, ShouldEqual, "Grand Theft Auto: Vice City Chaos%")
+	})
+
+	Convey("Fetch the category the variable belongs to", t, func() {
+		category, err := seedVar.Category()
+		So(err, ShouldBeNil)
+		So(category, ShouldBeNil)
+
+		variable, err := VariableByID("5lyjm9l4") // "Charatcer" in CTR
+		So(err, ShouldBeNil)
+		So(variable, ShouldNotBeNil)
+
+		category, err = variable.Category()
+		So(err, ShouldBeNil)
+		So(category, ShouldNotBeNil)
+		So(category.Name, ShouldEqual, "Any%")
+	})
+}
